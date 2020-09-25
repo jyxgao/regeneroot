273a0d987da782d29da9e09c2cb387ce5390fe79
Jenny Gao <gao.jenny9@gmail.com>
2020-09-25 05:48:40 +0000

    add dependencies

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const PORT       = process.env.PORT || 8080;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
 
@@ -23,12 +22,7 @@ app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
