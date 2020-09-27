8fc516bbcd93257a6e57a5fe09c8d5fdfb79fa0c
derekb123 <derekbutvin@gmail.com>
2020-09-27 05:47:43 +0000

    Maps base working, geocoder request recieving successful lat/long, hook not working yet

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import Geocoder from 'hooks/Geocoder';
 
 const MapContainer = () => {
+
+  const [ selected, setSelected ] = useState({});
   
+  const onSelect = item => {
+    setSelected(item);
+  }
+
+
+  const locations = [
+    {
+      name: "2100 208 Street",
+      location: { 
+        lat: 49.140780,
+        lng: -122.650860
+      },
+    },
+    {
+      name: "21479 Smith Crescent",
+      location: { 
+        lat: 49.1329347,
+        lng: -122.6294548
+      },
+    },
+    {
+      name: "5910 216 St",
+      location: { 
+        lat: 49.109517,
+        lng: -122.6260083
+      },
+    }
+  ];
+
   const mapStyles = {        
-    height: "70vh",
-    width: "70%"};
+    height: "100vh",
+    width: "100%"};
   
   const defaultCenter = {
-    lat: 49.2462, lng: -123.1162
+    lat: 49.1329347, lng: -122.6294548
   }
   
   return (
-     <LoadScript
-       googleMapsApiKey='AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'>
-        <GoogleMap
+    <LoadScript
+    googleMapsApiKey='AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'>
+     <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={13}
-          center={defaultCenter}
-        />
-     </LoadScript>
+          zoom={11}
+          center={defaultCenter}>
+        {
+            locations.map(item => {
+              return (
+              <Marker key={item.name} 
+                position={item.location}
+                onClick={() => {
+                  onSelect(item)
+                  Geocoder()
+                }}
+              />
+              )
+            })
+         }
+        {
+            selected.location && 
+            (
+              <InfoWindow
+              position={selected.location}
+              clickable={true}
+              onCloseClick={() => setSelected({})}
+            >
+              <p>{selected.name}</p>
+            </InfoWindow>
+            )
+         }
+     </GoogleMap>
+  </LoadScript>
   )
 }
 export default MapContainer;
