3e4aed5296e0bb841180e363b4eedcef9681237c
Jenny Gao <gao.jenny9@gmail.com>
2020-10-01 02:35:04 +0000

    styling to maplist - map popup

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -118,7 +118,7 @@ const Home = () => {
           </Pane>
         )}
       </Pane>
-
+    
       <Pane display="flex" flexDirection="row" flexWrap="wrap">
         {state.lots.map((lot) => {
           return (
