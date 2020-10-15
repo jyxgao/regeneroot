ea7e60073d646245ad5b458538f90a6514030075
Jenny Gao <gao.jenny9@gmail.com>
2020-10-15 22:43:25 +0000

    config for netlify deployment

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(
   <React.StrictMode>
